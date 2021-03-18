faacdc66934841215a92a5682930f1dd54f294e9
Andre Parsons <andrparsons@gmail.com>
2021-03-18 14:56:48 -0400

    created Jumbotron Component and added to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import react from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
+import Home from "./pages/home";
 
 export function App() {
   return (
@@ -16,7 +17,7 @@ export function App() {
           <p>Browse Page</p>
         </Route>
         <Route path="/">
-          <p>Application</p>
+          <Home />
         </Route>
       </Switch>
     </Router>

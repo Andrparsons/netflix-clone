f7b164a0e9a2fe3430939933f9ba1c21837cc843
Andre Parsons <andrparsons@gmail.com>
2021-03-20 15:32:16 -0400

    added global style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { GlobalStyles } from "./global-styles";
 import { App } from "./App";
 import "normalize.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById("root")
+);

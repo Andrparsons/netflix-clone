d51f13c25cc02f3aba8b9851fac3c930c2841723
Andre Parsons <andrparsons@gmail.com>
2021-03-31 17:15:54 -0400

    set up firebase / made signin page components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import Home from "./pages/home";
+import { Home, Signin } from "./pages";
 
 export function App() {
   return (
     <Router>
       <Switch>
         <Route path={ROUTES.SIGN_IN}>
-          <p>Sign in page</p>
+          <Signin />
         </Route>
         <Route path={ROUTES.SIGN_UP}>
           <p>Sign up page</p>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GlobalStyles } from "./global-styles";
 import { App } from "./App";
+import { FirebaseContext } from "./context/firebase";
 // import "normalize.css";
+
+const config = {
+  apiKey: "AIzaSyDH7hj5RDWgwlDmZI7adi77PgD9VnyezoM",
+  authDomain: "netflix-clone-29c32.firebaseapp.com",
+  dataBaseURL: "https://netflix-clone-29c32-default-rtdb.firebaseio.com/",
+  projectId: "netflix-clone-29c32",
+  storageBucket: "netflix-clone-29c32.appspot.com",
+  messagingSenderId: "107946305205",
+  appId: "1:107946305205:web:2a87b70e2e9dde6eeac0fc",
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
